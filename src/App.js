979bc02404453f0d2bd9aac3dddc1678d8806449
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2022-01-11 21:16:08 -0500

    fix for initial deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Container from './components/Container';
 import StocksRenderer from './components/StocksRenderer';
-import Stocks from './components/Stocks';
 import Search from './components/Search';
 
 function App() {
